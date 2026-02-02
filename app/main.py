764bb432eab1c2695172ac57ced6a7ed1529cd58
cicerodesousa1985-rgb <cicerodesousa1985@gmail.com>
2026-02-02 18:09:10 +0000

    Corrige main.py removendo comandos inválidos

--- app/main.py
+++ app/main.py
@@ -7,29 +7,3 @@ def home():
     return {
         "status": "Sistema de Checklist da Oficina rodando com sucesso"
     }
-python3 -m uvicorn app.main:app --host 0.0.0.0 --port 8000
-
-from fastapi import FastAPI
-
-app = FastAPI(title="Checklist Digital para Oficinas")
-
-@app.get("/")
-def home():
-    return {
-        "status": "Sistema de Checklist da Oficina rodando com sucesso"
-    }
-from fastapi import FastAPI
-
-app = FastAPI(title="Checklist Digital para Oficinas")
-
-@app.get("/")
-def home():
-    return {
-        "status": "Sistema de Checklist da Oficina rodando com sucesso"
-    }
-python3 -m uvicorn app.main:app --host 0.0.0.0 --port 8000
-
-git add app/main.py
-git commit -m "Corrige endpoint inicial"
-git push
-^
